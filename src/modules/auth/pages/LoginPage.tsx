33d887bd1d7bce479d35c678bffe022ddb0874f3
Diego Santivañez <Diegojsf17@gmail.com>
2024-11-05 02:22:33 -0500

    feat: frontend updated

--- src/modules/auth/pages/LoginPage.tsx
+++ src/modules/auth/pages/LoginPage.tsx
@@ -1,10 +1,11 @@
 import LoginForm from "../components/LoginForm.tsx";
+import logo from "../../../assets/logo.png";
 
 const LoginPage=() =>{
     return (
         <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <img className="mx-auto h-32 w-auto rounded-full" src="/src/assets/logo.png" alt="Code Showdown"/>
+                <img className="mx-auto h-32 w-auto rounded-full" src={logo} alt="Code Showdown"/>
                 <h1 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Login</h1>
                 <LoginForm/>
             </div>
